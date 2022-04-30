944c12f64b2f75217222e4762c997fc9caba0655
EarthenSkyLaptop <24978329+EarthenSky@users.noreply.github.com>
2022-04-29 21:05:01 -0700

    adding the outlines

--- src/python/outline-images.py
+++ src/python/outline-images.py
@@ -1,10 +1,19 @@
 # NOTE: do `pip install opencv-python` if you need to
 import cv2
 
-im = cv2.imread('a.png') 
+# NOTE: this script turns the regular pokemon images into blue outlines.
 
-# Make all pixels where Blue > 150 into white
-im[im[...,0]>150] = [255,255,255]
+# get pokemon names
 
-# Save result
-cv2.imwrite('result.png', im)
+with open("data/pokemon.txt", 'r') as f:
+   pokemon_list = map(lambda x: x.replace("\n", ""), f.readlines())
+
+# transform pokemon 
+
+for pkmn in pokemon_list:
+    img = cv2.imread("../../res/images/pkmn_normal/" + pkmn + ".png", cv2.IMREAD_UNCHANGED)
+
+    img[:,:,0] = 159
+    img[:,:,1] = 68
+    img[:,:,2] = 0
+    cv2.imwrite("../../res/images/pkmn_outline/" + pkmn + ".png", img)
